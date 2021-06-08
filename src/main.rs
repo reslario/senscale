2451175959a3ad7cd112907570e03bd46ebf5915
reslario <reslario.code@gmail.com>
2021-06-08 18:07:00 +0200

    Prepend "error:" when printing error

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ type Result<T = ()> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 fn main() {
     if let Err(e) = run() {
-        eprint!("{}", e)
+        eprint!("error: {}", e)
     }
 }
 
