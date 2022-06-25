ac710c5bc238cddeef41289d1590aec49b2aeeb4
reslario <reslario.code@gmail.com>
2022-06-25 23:03:12 +0200

    Test windows::util functions

--- src/windows/util.rs
+++ src/windows/util.rs
@@ -12,7 +12,30 @@ pub fn validate(result: i32) -> io::Result<()> {
 pub unsafe fn uninit_sized<T>(size_member: fn(&mut T) -> &mut u32) -> T {
     use std::mem::{MaybeUninit, size_of};
 
-    let mut info = MaybeUninit::<T>::uninit().assume_init();
-    *size_member(&mut info) = size_of::<T>() as _;
-    info
+    let mut value = MaybeUninit::<T>::uninit().assume_init();
+    *size_member(&mut value) = size_of::<T>() as _;
+    value
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn validate() {
+        assert!(super::validate(0).is_err());
+        assert!(super::validate(!0).is_ok())
+    }
+
+    #[test]
+    fn uninit_sized() {
+        struct Test {
+            size: u32,
+            #[allow(unused)]
+            whatever: u64
+        }
+
+        assert_eq!(
+            unsafe { super::uninit_sized::<Test>(|test| &mut test.size ) }.size,
+            std::mem::size_of::<Test>() as u32
+        )
+    }
 }
